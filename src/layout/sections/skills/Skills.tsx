4e7e7c042a5785c953083df1b15c0a1b1481472b
KorolenkoVadim <korolenkovadim@gmail.com>
2025-03-19 21:23:52 +0300

    skills

---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Icon} from "../../../components/icon/Icon.tsx";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Skill} from "./skill/Skill.tsx";
+
+export const Skills = () => {
+    return (
+        <StyledSkills>
+            <SectionTitle>My Skills</SectionTitle>
+            <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+                <Skill iconId={"code"}
+                       title={"html5"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"css"}
+                       title={"css3"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"react"}
+                       title={"React"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"typescript"}
+                       title={"typescript"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"stylecomp"}
+                       title={"styled components"}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"figma"}
+                       title={"WEB DESIgN  "}
+                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+            </FlexWrapper>
+
+        </StyledSkills>
+    );
+};
+
+const StyledSkills = styled.section`
+background-color: bisque;
+    min-height: 100vh;
+`
